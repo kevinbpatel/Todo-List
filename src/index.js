8787ef38548bc2caf9e98c3a3903130276ae32db
kevinbpatel <kevnpatel99@gmail.com>
2025-06-18 12:28:07 -0400

    Add sidebar with projects and add button
    Add sidebar with basic styling along with functions to re-render the
    elements within in.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,34 @@ import "./styles.css";
 import { todoItem, checkListItem } from "./todo-item.js";
 import { project } from "./project.js";
 
-
 // instantiate list of projects
 const projects = [];
 
+// sample data
 projects.push(project("new project"));
 projects[0].addItem(todoItem("first item"));
 projects[0].addItem(todoItem("second item"));
 projects[0].addItem(todoItem("third item"));
 
-projects[0].items.forEach(item => {
-  console.log(item.title);
+
+// display all projects
+const rerenderProjects = () => { 
+  const sidebar = document.querySelector("#sidebar-projects");
+  sidebar.replaceChildren();
+  projects.forEach(project => {
+    const projectLink = document.createElement("a");
+    projectLink.textContent = project.title;
+    sidebar.appendChild(projectLink);
+  });
+};
+
+// add project button
+const addProjectButton = document.createElement("button");
+addProjectButton.textContent = "+";
+sidebar.appendChild(addProjectButton);
+addProjectButton.addEventListener("click", () => { 
+  projects.push(project("Untitled"));
+  rerenderProjects();
 });
+
+rerenderProjects();
